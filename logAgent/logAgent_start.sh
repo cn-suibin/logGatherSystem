1d45d505fdfd21279826a67fef5657ffcda105b1
mr.sui <157595075@qq.com>
2020-12-16 23:08:52 +0800

    init

--- logAgent/logAgent_start.sh
+++ logAgent/logAgent_start.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 #read -p "日志采集. [y/n]: " sinstall
 #if [ $sinstall == 'y' ]; then
+basedir=`cd $(dirname $0); pwd -P`
+#输入
+read -p "请输入中控端IP: " sinstall
+if [ ! $sinstall ]; then
+echo "必须输入IP，启动退出"
+exit
+fi
+
+
 echo "关闭logAgent进程......"
 PID=`ps -ef |grep filebeat  |grep -v grep | awk '{print $2}'`
 echo $PID
@@ -20,17 +29,24 @@ if [ ! -d "filebeat-7.7.0-linux-x86_64" ]; then
 		 
 		echo "拷贝初始化配置filebeat.yml......"
 		cp filebeat.yml filebeat-7.7.0-linux-x86_64
-		
+		sed -i -e "s/mmmmm/$sinstall/g" filebeat-7.7.0-linux-x86_64/filebeat.yml
 		echo "创建动态配置目录"
 		mkdir filebeat-7.7.0-linux-x86_64/inputs.d
 		cp filebeat-demo.yml filebeat-7.7.0-linux-x86_64/inputs.d
-		inputs.d
 	fi
 fi
 wait
 echo "启动logAgent日志终端......"
-./filebeat-7.7.0-linux-x86_64/filebeat -c filebeat.yml &
+echo "拷贝初始化配置filebeat.yml......"
+#cp filebeat.yml filebeat-7.7.0-linux-x86_64
 
+#sed 's/mmmmm/'"$sinstall"'/g' filebeat-7.7.0-linux-x86_64/filebeat.yml 
+
+
+#cp filebeat-demo.yml filebeat-7.7.0-linux-x86_64/inputs.d
+
+$basedir/filebeat-7.7.0-linux-x86_64/filebeat -c $basedir/filebeat-7.7.0-linux-x86_64/filebeat.yml &
+echo "启动logAgent成功"
 #else 
 #exit
 #fi
